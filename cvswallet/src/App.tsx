55fb8721741b83caaf51da8e697d4d12313b877e
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-11-14 19:42:14 -0700

    Initial

---
+++ cvswallet/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AccountCreate from './scenes/AccountCreate';
+
+function App() {
+  return (
+    <div className="App">
+      <AccountCreate />
+    </div>
+  );
+}
+
+export default App;
---
+++ cvswallet/src/utils/AccountUtils.tsx
@@ -0,0 +1,16 @@
+import { ethers } from 'ethers';
+
+const generateKeys = (seedPhrase?: string) => {
+    let wallet: ethers.HDNodeWallet;
+
+    if (seedPhrase) {
+        wallet = ethers.HDNodeWallet.fromMnemonic(seedPhrase);
+    } else {
+        wallet = ethers.Wallet.createRandom();
+        seedPhrase = wallet.mnemonic?.phrase || undefined;
+    }
+
+    // Rest of your code...
+
+    return wallet; // Add any additional return statements as needed
+};
